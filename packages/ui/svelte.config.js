5f693f2e43e9276e0ea03612f8da867f0050ffd8
Colin Gastelle <crgastelle@gmail.com>
2025-07-05 20:07:44 -0600

    vercel.json adjustments

--- packages/ui/svelte.config.js
+++ packages/ui/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,13 +6,8 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-
 	kit: {
-		adapter: adapter({
-			pages: 'dist', // Output directory
-			assets: 'dist', // Assets directory
-			fallback: null
-		})
+		adapter: adapter()
 	}
 };
 
